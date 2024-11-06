65d6cbc664de696ac4fb7efdf293f06be397e7d7
SandaruwanWeerawardhana <weerawardanasw@gmail.com>
2024-11-06 11:22:35 +0530

    add array

---
+++ app.js
@@ -0,0 +1,60 @@
+// let Customer = {
+//     name:"Saman",
+//     age:20,
+//     address:"Kaluthara",
+//     Item:[
+//             {  ID:2001,
+//             name:"Sanath"
+//         },
+//         {
+//             ID:2002,
+//             name:"Sanatha"
+//         },
+//         { ID:2003,
+//             name:"Sanmath"
+//         },
+//         {   ID:2004,
+//             name:"iman"
+//         }
+//         Reports:[
+//             {Head:"good",body:"bad"},
+//         ]
+//     ]
+
+// }
+// Customer.Item.forEach(Item => {
+//     console.log(Customer.Item.Reports);
+    
+// });
+// console.log();
+
+// let CusList =[10,20,30,5,10,15,80,100];
+
+// console.log(CusList);
+// console.log(CusList.push(2));
+// console.log(CusList.sort());
+// console.log(CusList);
+
+let numbers = [10, 50, 68, 70, 95, 102, 305, 150, 72];
+let numbers2 = [8, 6, 8, 72];
+let numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let newAr = numbers.concat(numbers2,numbers3);
+
+console.log(newAr);
+console.log(newAr.sort());
+
+let char= ["f","s","g","t","y"]
+let char2= ["d","ssadkjh","jfgg","fggljt","yjhfg"]
+console.log(char);
+console.log(char2);
+
+let con=char.concat(char2)
+console.log(con);
+
+
+
+
+
+
+
